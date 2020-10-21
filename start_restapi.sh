55b1dd073cb40af8ebf3417b76a01842aaa2589d
vincentnam <dang.vincentnam@gmail.com>
2020-10-21 18:55:46 +0200

    fix sh

--- start_restapi.sh
+++ start_restapi.sh
@@ -2,4 +2,4 @@
 # PATH_TO_FLASK=/datalake/frontend/
 PATH_TO_FLASK=/data/python-project/
 sudo docker build -t rest_api .
-sudo docker run -p 5000:5000 -v REST-api_datalake/:/app/ -v /datalake/flask_tmp/:/flask_tmp rest_api
+sudo docker run -p 5000:5000 -v ${PATH_TO_FLASK}REST-api_datalake/:/app/ -v /datalake/flask_tmp/:/flask_tmp rest_api

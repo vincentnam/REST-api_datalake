b14bc10e69dd01816cbff6dfb2106f4ca9f67e43
Vincent-Nam DANG <vdang@co2-dl-nodejs.irit.fr>
2020-10-27 13:21:29 +0100

    git pull add to script : TODO -> change to docker-compose file

--- start_restapi.sh
+++ start_restapi.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 PATH_REACT_FOLDER=${pwd}
+git pull
 sudo docker build -t rest_api .
 sudo docker run -it -p 5000:5000 -v ${PATH_TO_FLASK}/:/app/ -v /datalake/flask_tmp/:/flask_tmp rest_api

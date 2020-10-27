798db72341555a2826520dcdc55c1ab80add8540
vincentnam <dang.vincentnam@gmail.com>
2020-10-27 18:01:24 +0100

    No 'Access-Control-Allow-Origin' header is present on the requested resource : try to fix

--- app.py
+++ app.py
@@ -10,28 +10,28 @@ import datetime
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
+#
+#
+# def build_preflight_response():
+#     response = make_response()
+#     response.headers.add("Access-Control-Allow-Origin", "*")
+#     response.headers.add('Access-Control-Allow-Headers', "*")
+#     response.headers.add('Access-Control-Allow-Methods', "*")
+#     return response
+#
+#
+# def build_actual_response(response):
+#     response.headers.add("Access-Control-Allow-Origin", "*")
+#
+#     return response
+#
 
-
-def build_preflight_response():
-    response = make_response()
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add('Access-Control-Allow-Headers', "*")
-    response.headers.add('Access-Control-Allow-Methods', "*")
-    return response
-
-
-def build_actual_response(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
-
-    return response
-
-
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', '*')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    return response
+# @app.after_request
+# def after_request(response):
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#     response.headers.add('Access-Control-Allow-Headers', '*')
+#     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+#     return response
 
 # @app.before_request
 # def append_cors_origin(response):

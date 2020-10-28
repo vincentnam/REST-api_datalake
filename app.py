57ad10427f222db2ac8d7868013b7395825aadac
vincentnam <dang.vincentnam@gmail.com>
2020-10-28 17:28:05 +0100

    implementation beginning of SLO upload file in Swift

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def upload_file():
     container_name = "test_ui-react"
     # check if the post request has the file part
     if 'file' not in request.files:
+        logger.info("CHAOCHAO")
         return jsonify(message="No file to upload")
     file = request.files["file"]
     logger.info(str(type(request.files["file"])))
